8b8c0c2314a5f63c49591272d2dc1375d45b282d
Chris <chris@clr.is>
2022-03-08 17:49:36 +0000

    Configuration classes. The `config.py` class now handles configuration for Flask. This is a far more stable and robust system, allowing expansion. We can also configure different environments for both development, debugging, and production.

---
+++ flask/config.py
@@ -0,0 +1,30 @@
+class Config(object):
+    DEBUG = False
+    DEVELOPMENT = False
+
+    # todo, load from file?
+    MYSQL_DATABASE_USER = 'root'
+    MYSQL_DATABASE_PASSWORD = ''
+    MYSQL_DATABASE_DB = 'engagement-db'
+    MYSQL_DATABASE_HOST = 'localhost'
+    MYSQL_DATABASE_CHARSET = 'utf8mb4'
+
+
+class DevConfig(Config):
+    DEBUG = True
+    DEVELOPMENT = True
+
+    MYSQL_DATABASE_USER = 'root'
+    MYSQL_DATABASE_PASSWORD = ''
+    MYSQL_DATABASE_DB = 'engagement-db'
+    MYSQL_DATABASE_HOST = 'localhost'
+    MYSQL_DATABASE_CHARSET = 'utf8mb4'
+
+
+config = {
+    'default': Config,
+    'config': Config,
+    'dev': DevConfig
+}
+
+get_config = lambda key: config.get(key or 'default', config.get('default'))
