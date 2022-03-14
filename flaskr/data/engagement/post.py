b577f8a8f48ae0f0c27aaf6494ca76f002a38b43
Chris <chris@clr.is>
2022-03-14 21:42:49 +0000

    All finished with back-end.

--- flaskr/data/engagement/post.py
+++ flaskr/data/engagement/post.py
@@ -1,8 +1,10 @@
 class EngagementPost:
-    def __init__(self, service_id, author, content, posted, found, url, post_id=-1):
+    def __init__(self, service_id, service, univ, author, content, posted, found, url, post_id=-1):
         # post_id having val -1 means that it has been constructed and not pulled from db.
         self.post_id = post_id
         self.service_id = service_id
+        self.service = service
+        self.univ = univ
         self.author = author
         self.content = content
         self.posted = posted
@@ -12,7 +14,7 @@ class EngagementPost:
     @staticmethod
     def from_row(row):
         return EngagementPost(
-            row[1], row[2], row[3], row[4], row[5], row[6],
+            row[1], 'Twitter', row[7], row[2], row[3], row[4], row[5], row[6],
             post_id=row[0]
         )
 
@@ -20,6 +22,8 @@ class EngagementPost:
         return {
             'post_id': self.post_id,
             'service_id': self.service_id,
+            'service': self.service,
+            'univ': self.univ.capitalize(),
             'author': self.author,
             'content': self.content,
             'posted': self.posted,
