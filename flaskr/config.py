d78a1926551940488a13c0ca1920f29d56f774fc
Rathin Shah <rathinkshah2003@gmail.com>
2022-03-17 10:12:57 +0400

    Added port number and instructions in Config file

--- flaskr/config.py
+++ flaskr/config.py
@@ -4,10 +4,16 @@ class Config(object):
 
     # todo, load from file?
     MYSQL_DATABASE_USER = 'root'
+
+    #Enter password here
     MYSQL_DATABASE_PASSWORD = ''
+    
     MYSQL_DATABASE_DB = 'engagement-db'
     MYSQL_DATABASE_HOST = 'localhost'
     MYSQL_DATABASE_CHARSET = 'utf8mb4'
+    
+    # Change this if a different port number is used
+    MYSQL_DATABASE_PORT = 3306
 
 
 class DevConfig(Config):
